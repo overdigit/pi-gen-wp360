9ee40d0f509d5e0b0bff9f10545d451c56862ffd
Nixola <niki.or97@yahoo.it>
2025-04-23 23:28:30 +0200

    WP360 goodies

---
+++ stagewp360/01-webvisu/01-run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+install -m 755 -d "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/"
+install -m 644 files/autologin.conf "${ROOTFS_DIR}/etc/systemd/system/getty@tty1.service.d/autologin.conf"
+install -m 644 files/sway.service "${ROOTFS_DIR}/etc/systemd/user/sway.service"
+install -m 755 -d "${ROOTFS_DIR}/etc/sway/config.d/"
+install -m 644 files/51-systemd-user-target.conf "${ROOTFS_DIR}/etc/sway/config.d/51-systemd-user-target.conf"
+install -m 644 files/sway-session.target "${ROOTFS_DIR}/etc/systemd/user/sway-session.target"
+install -m 644 files/codesys.target "${ROOTFS_DIR}/etc/systemd/user/codesys.target"
+install -m 644 files/webvisu.service "${ROOTFS_DIR}/etc/systemd/user/webvisu.service"
+
+on_chroot <<- \EOF
+  systemctl --user --machine=user@ enable sway
+  systemctl --user --machine=user@ enable webvisu
+EOF
