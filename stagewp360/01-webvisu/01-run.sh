768688f4fc9d830ca5b1916b0f557cefc1087fdd
Nicola Orlando <nicolaorlando24@gmail.com>
2025-05-07 18:18:30 +0200

    Move codesys user target to systemd service mask

--- stagewp360/01-webvisu/01-run.sh
+++ stagewp360/01-webvisu/01-run.sh
@@ -8,6 +8,10 @@ install -m 644 files/51-systemd-user-target.conf "${ROOTFS_DIR}/etc/sway/config.
 install -m 644 files/sway-session.target "${ROOTFS_DIR}/etc/systemd/user/sway-session.target"
 install -m 644 files/codesys.target "${ROOTFS_DIR}/etc/systemd/user/codesys.target"
 install -m 644 files/webvisu.service "${ROOTFS_DIR}/etc/systemd/user/webvisu.service"
+install -m 755 -d "${ROOTFS_DIR}/etc/systemd/system/codesyscontrol.service.d/"
+install -m 644 files/10-codesys-user-target.conf "${ROOTFS_DIR}/etc/systemd/system/codesyscontrol.service.d/10-codesys-user-target.conf"
+
+sed -i 's/%USER%/'"${FIRST_USER_NAME}"'/g' "${ROOTFS_DIR}/etc/systemd/system/codesyscontrol.service.d/10-codesys-user-target.conf"
 
 on_chroot <<- \EOF
   sudo -u $FIRST_USER_NAME systemctl --user enable sway
