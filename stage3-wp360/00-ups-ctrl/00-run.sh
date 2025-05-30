13c7e25966522f369e0f7e0feb2b90fbfe30f71d
Nicola Orlando <niki.or97@yahoo.it>
2025-05-30 19:52:29 +0200

    Changed .deb files naming scheme This is all temporary until we set up a proper repo anyway

--- stage3-wp360/00-ups-ctrl/00-run.sh
+++ stage3-wp360/00-ups-ctrl/00-run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-install -m 644 files/wp360-ups-ctrl-1.0.0.deb "${ROOTFS_DIR}/"
+install -m 644 files/wp360-ups-ctrl.deb "${ROOTFS_DIR}/"
 
 on_chroot <<- \EOF
-  dpkg -i /wp360-ups-ctrl-1.0.0.deb
-  rm /wp360-ups-ctrl-1.0.0.deb
+  dpkg -i /wp360-ups-ctrl.deb
+  rm /wp360-ups-ctrl.deb
 EOF
--- stage5-webvisu/00-webvisu/01-run.sh
+++ stage5-webvisu/00-webvisu/01-run.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -e
 
-#dpkg --root "${ROOTFS_DIR}/" -i "files/wp360-webvisu-1.0.0.deb"
-install -m 644 files/wp360-webvisu-1.0.0.deb "${ROOTFS_DIR}/"
+install -m 644 files/wp360-webvisu.deb "${ROOTFS_DIR}/"
 
 on_chroot <<- \EOF
-  dpkg -i /wp360-webvisu-1.0.0.deb
-  rm /wp360-webvisu-1.0.0.deb
+  dpkg -i /wp360-webvisu.deb
+  rm /wp360-webvisu.deb
 
   sudo -u $FIRST_USER_NAME systemctl --user enable sway
   sudo -u $FIRST_USER_NAME systemctl --user enable webvisu
