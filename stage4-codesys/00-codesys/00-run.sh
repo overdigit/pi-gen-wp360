2d2d765e104782cf104fe9e33d4dc6ff29ad2dca
Nicola Orlando <niki.or97@yahoo.it>
2025-06-19 17:55:02 +0200

    Update CODESYS runtime to v4.16.0.0

--- stage4-codesys/00-codesys/00-run.sh
+++ stage4-codesys/00-codesys/00-run.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -e
 
 install -m 644 files/codemeter-lite_8.20.6539.500_arm64.deb "${ROOTFS_DIR}/"
-install -m 644 files/codesyscontrol_linuxarm64_4.15.0.0_arm64.deb "${ROOTFS_DIR}/"
-install -m 644 files/codesysedge_edgearm64_4.15.0.0_arm64.deb "${ROOTFS_DIR}/"
+install -m 644 files/codesyscontrol_linuxarm64_4.16.0.0_arm64.deb "${ROOTFS_DIR}/"
+install -m 644 files/codesysedge_edgearm64_4.16.0.0_arm64.deb "${ROOTFS_DIR}/"
 
 on_chroot <<- \EOF
 # CONTAINER prevents these packages from starting their service on configure
   export CONTAINER=true
   dpkg -i /codemeter-lite_8.20.6539.500_arm64.deb
-  dpkg -i /codesyscontrol_linuxarm64_4.15.0.0_arm64.deb
-  dpkg -i /codesysedge_edgearm64_4.15.0.0_arm64.deb
+  dpkg -i /codesyscontrol_linuxarm64_4.16.0.0_arm64.deb
+  dpkg -i /codesysedge_edgearm64_4.16.0.0_arm64.deb
   rm /codemeter-lite_8.20.6539.500_arm64.deb
-  rm /codesyscontrol_linuxarm64_4.15.0.0_arm64.deb
-  rm /codesysedge_edgearm64_4.15.0.0_arm64.deb
+  rm /codesyscontrol_linuxarm64_4.16.0.0_arm64.deb
+  rm /codesysedge_edgearm64_4.16.0.0_arm64.deb
 EOF
 
 cat >> "${ROOTFS_DIR}/etc/codesyscontrol/CODESYSControl_User.cfg" <<- \EOF
