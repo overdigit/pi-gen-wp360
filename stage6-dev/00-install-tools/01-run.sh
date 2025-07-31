01e3a26d228d5fbffada0751ead7038ac6f67612
Nicola Orlando <niki.or97@yahoo.it>
2025-07-31 19:23:30 +0200

    Added dev utils stage

---
+++ stage6-dev/00-install-tools/01-run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+install -m 644 files/clock.sh   "${ROOTFS_DIR}/home/user/"
+install -m 644 files/avg.lua    "${ROOTFS_DIR}/home/user/"
+install -m 644 files/graph.lua  "${ROOTFS_DIR}/home/user/"
+install -m 644 files/run.screen "${ROOTFS_DIR}/home/user/"
