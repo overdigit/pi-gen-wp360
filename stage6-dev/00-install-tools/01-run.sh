704c21efbe48bf54683a05be012cb5bb908fd563
Nicola Orlando <niki.or97@yahoo.it>
2025-09-25 18:20:50 +0200

    stage6-dev: fix test tools install script

--- stage6-dev/00-install-tools/01-run.sh
+++ stage6-dev/00-install-tools/01-run.sh
@@ -2,9 +2,9 @@
 
 PKGNAME=wp360-test-tools
 
-install -m 644 files/${PKGNAME}.deb "${ROOTFS_DIR}/"
+install -m 644 files/wp360-test-tools.deb "${ROOTFS_DIR}/"
 
 on_chroot <<- \EOF
-  apt install /${PKGNAME}.deb
-  rm /${PKGNAME}.deb
+  dpkg -i /wp360-test-tools.deb
+  rm /wp360-test-tools.deb
 EOF
