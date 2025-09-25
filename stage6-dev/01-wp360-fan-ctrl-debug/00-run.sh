fd7e54a960494383bf235f38453a1597b05790e1
Nicola Orlando <niki.or97@yahoo.it>
2025-09-25 18:21:48 +0200

    stage6-dev: fix fan ctrl debug script typo

--- stage6-dev/01-wp360-fan-ctrl-debug/00-run.sh
+++ stage6-dev/01-wp360-fan-ctrl-debug/00-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-install -m 755 "${ROOTFS_DIR}/etc/systemd/system/wp360-fan-ctrl.d/
+install -d 755 "${ROOTFS_DIR}/etc/systemd/system/wp360-fan-ctrl.d/"
 install -m 644 files/debug.conf   "${ROOTFS_DIR}/etc/systemd/system/wp360-fan-ctrl.d/debug.conf"
