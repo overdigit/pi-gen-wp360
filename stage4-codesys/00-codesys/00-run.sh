4d4b1ed381779bd0fdfe7ee1581be32110d3545b
Nicola Orlando <niki.or97@yahoo.it>
2025-10-16 17:43:40 +0200

    stage4-codesys: fix another mispaste

--- stage4-codesys/00-codesys/00-run.sh
+++ stage4-codesys/00-codesys/00-run.sh
@@ -2,17 +2,17 @@
 
 install -m 644 files/codemeter-lite_8.30.6885.501_arm64.deb "${ROOTFS_DIR}/"
 install -m 644 files/codesyscontrol_linuxarm64_4.17.0.0_arm64.deb "${ROOTFS_DIR}/"
-install -m 644 files/codesyscontrol_linuxarm64_4.17.0.0_arm64.deb "${ROOTFS_DIR}/"
+install -m 644 files/codesysedge_edgearm64_4.17.0.0_arm64.deb "${ROOTFS_DIR}/"
 
 on_chroot <<- \EOF
 # CONTAINER prevents these packages from starting their service on configure
   export CONTAINER=true
   dpkg -i /codemeter-lite_8.30.6885.501_arm64.deb
   dpkg -i /codesyscontrol_linuxarm64_4.17.0.0_arm64.deb
-  dpkg -i /codesyscontrol_linuxarm64_4.17.0.0_arm64.deb
+  dpkg -i /codesysedge_edgearm64_4.17.0.0_arm64.deb
   rm /codemeter-lite_8.30.6885.501_arm64.deb
   rm /codesyscontrol_linuxarm64_4.17.0.0_arm64.deb
-  rm /codesyscontrol_linuxarm64_4.17.0.0_arm64.deb
+  rm /codesysedge_edgearm64_4.17.0.0_arm64.deb
 EOF
 
 cat >> "${ROOTFS_DIR}/etc/codesyscontrol/CODESYSControl_User.cfg" <<- \EOF
