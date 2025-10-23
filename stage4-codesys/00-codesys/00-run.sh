cf32f58655cf67aed475e42c31c336310f54ada4
Nicola Orlando <niki.or97@yahoo.it>
2025-10-23 13:54:11 +0200

    Update Codesys runtime to 4.18.0.0

--- stage4-codesys/00-codesys/00-run.sh
+++ stage4-codesys/00-codesys/00-run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 install -m 644 files/codemeter-lite_8.30.6885.501_arm64.deb "${ROOTFS_DIR}/"
-install -m 644 files/codesyscontrol_linuxarm64_4.17.0.0_arm64.deb "${ROOTFS_DIR}/"
-install -m 644 files/codesysedge_edgearm64_4.17.0.0_arm64.deb "${ROOTFS_DIR}/"
+install -m 644 files/codesyscontrol_linuxarm64_4.18.0.0_arm64.deb "${ROOTFS_DIR}/"
+install -m 644 files/codesysedge_edgearm64_4.18.0.0_arm64.deb "${ROOTFS_DIR}/"
 
 on_chroot <<- \EOF
 # CONTAINER prevents these packages from starting their service on configure
