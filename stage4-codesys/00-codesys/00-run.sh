d9a538f5d278f2b46a3c308fc0201ea4e77ee705
Nicola Orlando <niki.or97@yahoo.it>
2025-11-03 16:14:00 +0100

    Actually update Codesys runtime to 4.18.0.0

--- stage4-codesys/00-codesys/00-run.sh
+++ stage4-codesys/00-codesys/00-run.sh
@@ -8,11 +8,11 @@ on_chroot <<- \EOF
 # CONTAINER prevents these packages from starting their service on configure
   export CONTAINER=true
   dpkg -i /codemeter-lite_8.30.6885.501_arm64.deb
-  dpkg -i /codesyscontrol_linuxarm64_4.17.0.0_arm64.deb
-  dpkg -i /codesysedge_edgearm64_4.17.0.0_arm64.deb
+  dpkg -i /codesyscontrol_linuxarm64_4.18.0.0_arm64.deb
+  dpkg -i /codesysedge_edgearm64_4.18.0.0_arm64.deb
   rm /codemeter-lite_8.30.6885.501_arm64.deb
-  rm /codesyscontrol_linuxarm64_4.17.0.0_arm64.deb
-  rm /codesysedge_edgearm64_4.17.0.0_arm64.deb
+  rm /codesyscontrol_linuxarm64_4.18.0.0_arm64.deb
+  rm /codesysedge_edgearm64_4.18.0.0_arm64.deb
 EOF
 
 cat >> "${ROOTFS_DIR}/etc/codesyscontrol/CODESYSControl_User.cfg" <<- \EOF
