c5cecfe2e09a12a544faacd04a1fa7152339864b
Nicola Orlando <niki.or97@yahoo.it>
2025-11-11 15:26:49 +0100

    stage5: move username handling for codesys user target to own package

--- stage5-webvisu/00-webvisu/01-run.sh
+++ stage5-webvisu/00-webvisu/01-run.sh
@@ -4,5 +4,3 @@ on_chroot <<- \EOF
   sudo -u $FIRST_USER_NAME systemctl --user enable sway
   sudo -u $FIRST_USER_NAME systemctl --user enable webvisu
 EOF
-
-sed -i 's/%USER%/'"${FIRST_USER_NAME}"'/g' "${ROOTFS_DIR}/usr/lib/systemd/system/codesyscontrol.service.d/10-codesys-user-target.conf"
