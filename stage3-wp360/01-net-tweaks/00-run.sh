214a5897b7f2e77101c446715a1c6738f3e7a470
Nicola Orlando <niki.or97@yahoo.it>
2026-01-13 10:58:30 +0100

    stage3-wp360: rename `static` connection to `eth0`

--- stage3-wp360/01-net-tweaks/00-run.sh
+++ stage3-wp360/01-net-tweaks/00-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-install -m 600 files/static.nmconnection "${ROOTFS_DIR}/etc/NetworkManager/system-connections/static.nmconnection"
-sed "s/uuid=/uuid=$(uuidgen)/" -i "${ROOTFS_DIR}/etc/NetworkManager/system-connections/static.nmconnection"
+install -m 600 files/eth0.nmconnection "${ROOTFS_DIR}/etc/NetworkManager/system-connections/eth0.nmconnection"
+sed "s/uuid=/uuid=$(uuidgen)/" -i "${ROOTFS_DIR}/etc/NetworkManager/system-connections/eth0.nmconnection"
