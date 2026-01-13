c835c0c0091dfb9dad8625c607721d9d949a47c1
Nicola Orlando <niki.or97@yahoo.it>
2026-01-13 11:23:43 +0100

    stage3-wp360: add stub connections for eth1 through 3

--- stage3-wp360/01-net-tweaks/00-run.sh
+++ stage3-wp360/01-net-tweaks/00-run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 install -m 600 files/eth0.nmconnection "${ROOTFS_DIR}/etc/NetworkManager/system-connections/eth0.nmconnection"
+
+for i in $(seq 1 3)
+do
+    install -m 600 files/ethN.nmconnection "${ROOTFS_DIR}/etc/NetworkManager/system-connections/eth${i}.nmconnection"
+    sed "s/ethN/eth${i}/" -i "${ROOTFS_DIR}/etc/NetworkManager/system-connections/eth${i}.nmconnection"
+    sed "s/uuid=/uuid=$(uuidgen)/" -i "${ROOTFS_DIR}/etc/NetworkManager/system-connections/eth${i}.nmconnection"
+done
+
 sed "s/uuid=/uuid=$(uuidgen)/" -i "${ROOTFS_DIR}/etc/NetworkManager/system-connections/eth0.nmconnection"
