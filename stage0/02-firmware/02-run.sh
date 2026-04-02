8a677fc2dca3b9b407189d5f4e5cd72cffca1d4a
Nicola Orlando <niki.or97@yahoo.it>
2026-04-02 12:41:13 +0200

    stage0: unhold Linux (Chromium+PREEMPT_RT bug should be fixed)

--- stage0/02-firmware/02-run.sh
+++ stage0/02-firmware/02-run.sh
@@ -8,8 +8,3 @@ if [ ! -f "${ROOTFS_DIR}/etc/kernel-img.conf" ]; then
 	echo "do_symlinks=0" > "${ROOTFS_DIR}/etc/kernel-img.conf"
 fi
 rm -f "${ROOTFS_DIR}/"{vmlinuz,initrd.img}*
-
-on_chroot <<- \EOF
-apt-mark hold linux-image-rpi-v8-rt
-apt-mark hold linux-headers-rpi-v8-rt
-EOF
