ab9526bc6d6e180b6e5051377f9d1c0565803844
Nicola Orlando <niki.or97@yahoo.it>
2026-06-25 16:44:19 +0200

    Compute and set default password at first boot

---
+++ stage3-wp360/03-set-pw/00-run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+install -d -m 755 "${ROOTFS_DIR}/usr/local/share/set_pw"
+install -m 755 files/set_pw "${ROOTFS_DIR}/usr/local/share/set_pw/"
+
+install -d -m 755 "${ROOTFS_DIR}/usr/local/lib/systemd/system/"
+install -m 644 files/set_pw.service "${ROOTFS_DIR}/usr/local/lib/systemd/system/"
+
+install -d -m 755 "${ROOTFS_DIR}/etc/systemd/system/default.target.wants/"
+ln -s /usr/local/lib/systemd/system/set_pw.service "${ROOTFS_DIR}/etc/systemd/system/default.target.wants/"
