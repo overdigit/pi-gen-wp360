982de7b938105ee033584a9efd87d97f98316e11
Nicola Orlando <niki.or97@yahoo.it>
2026-08-05 17:52:56 +0200

    stage4: update codesys

--- stage4-codesys/00-codesys/00-run.sh
+++ stage4-codesys/00-codesys/00-run.sh
@@ -1,21 +1,38 @@
 #!/bin/bash -e
-
 install -m 644 files/codemeter-lite_8.40.7131.502_arm64.deb "${ROOTFS_DIR}/"
-install -m 644 files/codesyscontrol_linuxarm64_4.20.0.0_arm64.deb "${ROOTFS_DIR}/"
-install -m 644 files/codesysedge_edgearm64_4.20.0.0_arm64.deb "${ROOTFS_DIR}/"
+install -m 644 files/codesyscontrol_linuxarm64_4.21.0.0_arm64.deb "${ROOTFS_DIR}/"
+install -m 644 files/codesysedge_edgearm64_4.21.0.0_arm64.deb "${ROOTFS_DIR}/"
+install -m 644 files/codesysproxy_proxyarm64_4.21.0.0_arm64.deb "${ROOTFS_DIR}/"
 
 on_chroot <<- \EOF
-# CONTAINER prevents these packages from starting their service on configure
-  export CONTAINER=true
   dpkg -i /codemeter-lite_8.40.7131.502_arm64.deb
-  dpkg -i /codesyscontrol_linuxarm64_4.20.0.0_arm64.deb
-  dpkg -i /codesysedge_edgearm64_4.20.0.0_arm64.deb
+  dpkg -i /codesyscontrol_linuxarm64_4.21.0.0_arm64.deb
+  dpkg -i /codesysedge_edgearm64_4.21.0.0_arm64.deb
+  dpkg -i /codesysproxy_proxyarm64_4.21.0.0_arm64.deb
+
+  set +e
+  # Let's try to stop CodeMeter before going on
+  start-stop-daemon --stop --quiet --retry=TERM/5/KILL/5 --exec /usr/sbin/CodeMeterLin
+  echo "Return code CodeMeterLin: $?"
+  start-stop-daemon --stop --quiet --retry=TERM/5/KILL/5 --exec /usr/lib/CodeMeter
+  echo "Return code CodeMeter: $?"
+  while pgrep CodeMeter
+  do
+    echo "Killing CodeMeter..."
+    pkill -SIGTERM CodeMeter
+    sleep 2s
+  done
+  set -e
+
   rm /codemeter-lite_8.40.7131.502_arm64.deb
-  rm /codesyscontrol_linuxarm64_4.20.0.0_arm64.deb
-  rm /codesysedge_edgearm64_4.20.0.0_arm64.deb
+  rm /codesyscontrol_linuxarm64_4.21.0.0_arm64.deb
+  rm /codesysedge_edgearm64_4.21.0.0_arm64.deb
+  rm /codesysproxy_proxyarm64_4.21.0.0_arm64.deb
+
+  usermod -aG dialout,gpio codesyscontrol
 EOF
 
-on_chroot <<- \EOF
-  systemctl enable codesyscontrol
-  systemctl enable codesysedge
-EOF
+#on_chroot <<- \EOF
+#  systemctl enable codesyscontrol
+#  systemctl enable codesysedge
+#EOF
