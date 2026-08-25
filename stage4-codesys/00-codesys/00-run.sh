7f5332b64b36f5d7707cf60603f6a720369a7fbc
Nicola Orlando <niki.or97@yahoo.it>
2026-08-25 15:33:07 +0200

    stage4: update codesys

--- stage4-codesys/00-codesys/00-run.sh
+++ stage4-codesys/00-codesys/00-run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 install -m 644 files/codemeter-lite_8.40.7131.502_arm64.deb "${ROOTFS_DIR}/"
-install -m 644 files/codesyscontrol_linuxarm64_4.21.0.0_arm64.deb "${ROOTFS_DIR}/"
-install -m 644 files/codesysedge_edgearm64_4.21.0.0_arm64.deb "${ROOTFS_DIR}/"
-install -m 644 files/codesysproxy_proxyarm64_4.21.0.0_arm64.deb "${ROOTFS_DIR}/"
+install -m 644 files/codesyscontrol_linuxarm64_4.22.0.0_arm64.deb "${ROOTFS_DIR}/"
+install -m 644 files/codesysedge_edgearm64_4.22.0.0_arm64.deb "${ROOTFS_DIR}/"
+install -m 644 files/codesysproxy_proxyarm64_4.22.0.0_arm64.deb "${ROOTFS_DIR}/"
 
 on_chroot <<- \EOF
   dpkg -i /codemeter-lite_8.40.7131.502_arm64.deb
-  dpkg -i /codesyscontrol_linuxarm64_4.21.0.0_arm64.deb
-  dpkg -i /codesysedge_edgearm64_4.21.0.0_arm64.deb
-  dpkg -i /codesysproxy_proxyarm64_4.21.0.0_arm64.deb
+  dpkg -i /codesyscontrol_linuxarm64_4.22.0.0_arm64.deb
+  dpkg -i /codesysedge_edgearm64_4.22.0.0_arm64.deb
+  dpkg -i /codesysproxy_proxyarm64_4.22.0.0_arm64.deb
 
   set +e
   # Let's try to stop CodeMeter before going on
@@ -25,9 +25,9 @@ on_chroot <<- \EOF
   set -e
 
   rm /codemeter-lite_8.40.7131.502_arm64.deb
-  rm /codesyscontrol_linuxarm64_4.21.0.0_arm64.deb
-  rm /codesysedge_edgearm64_4.21.0.0_arm64.deb
-  rm /codesysproxy_proxyarm64_4.21.0.0_arm64.deb
+  rm /codesyscontrol_linuxarm64_4.22.0.0_arm64.deb
+  rm /codesysedge_edgearm64_4.22.0.0_arm64.deb
+  rm /codesysproxy_proxyarm64_4.22.0.0_arm64.deb
 
   usermod -aG dialout,gpio codesyscontrol
 EOF
